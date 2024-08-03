5c8b74d9ee802efa1c2e19563e6c0c0a442c1e04
iman hashemi <hashemi.iman@gmail.com>
2024-08-03 22:33:41 +0330

    seasons

--- Modules/Course/app/Livewire/CourseDetail.php
+++ Modules/Course/app/Livewire/CourseDetail.php
@@ -3,15 +3,51 @@
 namespace Modules\Course\Livewire;
 
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Rule;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Modules\Course\Models\Season;
 
 class CourseDetail extends Component
 {
 
+    public $course_id;
+    #[Rule('required')]
+    public $title;
+
+    public function mount($course_id)
+    {
+        $this->course_id = $course_id;
+    }
+
+    public function addSeason()
+    {
+        $season =Season::query()->where('course_id', $this->course_id)->latest()->first();
+        if($season){
+            Season::query()->create([
+                'user_id'=>auth()->user()->id,
+                'course_id'=>$this->course_id,
+                'title'=>$this->title,
+                'priority'=>$season->priority+1,
+            ]);
+        }else{
+            Season::query()->create([
+                'user_id'=>auth()->user()->id,
+                'course_id'=>$this->course_id,
+                'title'=>$this->title,
+                'priority'=>1,
+            ]);
+        }
+
+        $this->dispatch('closeSeasonModal');
+
+
+    }
     #[Layout('panel::layouts.app'),Title('جزئیات دوره')]
     public function render()
     {
-        return view('course::livewire.course-detail');
+        $seasons = Season::query()->where('course_id', $this->course_id)
+            ->orderBy('priority','ASC')->get();
+        return view('course::livewire.course-detail', compact('seasons'));
     }
 }
--- Modules/Course/resources/views/livewire/course-detail.blade.php
+++ Modules/Course/resources/views/livewire/course-detail.blade.php
@@ -10,106 +10,69 @@
             <!-- Add lecture Modal button -->
             <div class="d-sm-flex justify-content-sm-between align-items-center mb-3">
                 <h5 class="mb-2 mb-sm-0">آپلود جلسات دوره</h5>
-                <a href="#" class="btn btn-sm btn-primary-soft mb-0" data-bs-toggle="modal" data-bs-target="#addLecture"><i class="bi bi-plus-circle me-2"></i>افزودن فصل</a>
+                <a href="#" class="btn btn-sm btn-primary-soft mb-0" data-bs-toggle="modal" data-bs-target="#addSeason"><i class="bi bi-plus-circle me-2"></i>افزودن فصل</a>
             </div>
 
             <!-- Edit lecture START -->
             <div class="accordion accordion-icon accordion-bg-light" id="accordionExample2">
-                <!-- Item START -->
-                <div class="accordion-item mb-3">
-                    <h6 class="accordion-header font-base" id="heading-1">
-                        <button class="accordion-button fw-bold rounded d-inline-block collapsed d-block pe-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false" aria-controls="collapse-1">
-                            معرفی دوره فتوشاپ
-                        </button>
-                    </h6>
+                @foreach($seasons as $season)
+                    <!-- Item START -->
+                    <div class="accordion-item mb-3">
+                        <h6 class="accordion-header font-base" id="heading-1">
+                            <button class="accordion-button fw-bold rounded d-inline-block collapsed d-block pe-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false" aria-controls="collapse-1">
+                                معرفی دوره فتوشاپ
+                            </button>
+                        </h6>
 
-                    <div id="collapse-1" class="accordion-collapse collapse show" aria-labelledby="heading-1" data-bs-parent="#accordionExample2">
-                        <!-- Topic START -->
-                        <div class="accordion-body mt-3">
-                            <!-- Video item START -->
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="position-relative">
-                                    <a href="#" class="btn btn-danger-soft btn-round btn-sm mb-0 stretched-link position-static"><i class="fas fa-play"></i></a>
-                                    <span class="ms-2 mb-0 h6 fw-light">معرفی دوره</span>
+                        <div id="collapse-1" class="accordion-collapse collapse show" aria-labelledby="heading-1" data-bs-parent="#accordionExample2">
+                            <!-- Topic START -->
+                            <div class="accordion-body mt-3">
+                                <!-- Video item START -->
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="position-relative">
+                                        <a href="#" class="btn btn-danger-soft btn-round btn-sm mb-0 stretched-link position-static"><i class="fas fa-play"></i></a>
+                                        <span class="ms-2 mb-0 h6 fw-light">معرفی دوره</span>
+                                    </div>
+                                    <!-- Edit and cancel button -->
+                                    <div>
+                                        <a href="#" class="btn btn-sm btn-success-soft btn-round me-1 mb-1 mb-md-0"><i class="far fa-fw fa-edit"></i></a>
+                                        <button class="btn btn-sm btn-danger-soft btn-round mb-0"><i class="fas fa-fw fa-times"></i></button>
+                                    </div>
                                 </div>
-                                <!-- Edit and cancel button -->
-                                <div>
-                                    <a href="#" class="btn btn-sm btn-success-soft btn-round me-1 mb-1 mb-md-0"><i class="far fa-fw fa-edit"></i></a>
-                                    <button class="btn btn-sm btn-danger-soft btn-round mb-0"><i class="fas fa-fw fa-times"></i></button>
+                                <!-- Divider -->
+                                <hr>
+                                <!-- Video item END -->
+
+                                <!-- Video item START -->
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="position-relative">
+                                        <a href="#" class="btn btn-danger-soft btn-round btn-sm mb-0 stretched-link position-static"><i class="fas fa-play"></i></a>
+                                        <span class="ms-2 mb-0 h6 fw-light">دیجیتال مارکتنیگ چیست؟</span>
+                                    </div>
+                                    <!-- Edit and cancel button -->
+                                    <div>
+                                        <a href="#" class="btn btn-sm btn-success-soft btn-round me-1 mb-1 mb-md-0"><i class="far fa-fw fa-edit"></i></a>
+                                        <button class="btn btn-sm btn-danger-soft btn-round mb-0"><i class="fas fa-fw fa-times"></i></button>
+                                    </div>
                                 </div>
+                                <!-- Divider -->
+                                <hr>
+                                <!-- Video item END -->
+
+                                <!-- Add topic -->
+                                <a href="#" class="btn btn-sm btn-dark mb-0" data-bs-toggle="modal" data-bs-target="#addTopic"><i class="bi bi-plus-circle me-2"></i>افزودن</a>
                             </div>
-                            <!-- Divider -->
-                            <hr>
-                            <!-- Video item END -->
-
-                            <!-- Video item START -->
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="position-relative">
-                                    <a href="#" class="btn btn-danger-soft btn-round btn-sm mb-0 stretched-link position-static"><i class="fas fa-play"></i></a>
-                                    <span class="ms-2 mb-0 h6 fw-light">دیجیتال مارکتنیگ چیست؟</span>
-                                </div>
-                                <!-- Edit and cancel button -->
-                                <div>
-                                    <a href="#" class="btn btn-sm btn-success-soft btn-round me-1 mb-1 mb-md-0"><i class="far fa-fw fa-edit"></i></a>
-                                    <button class="btn btn-sm btn-danger-soft btn-round mb-0"><i class="fas fa-fw fa-times"></i></button>
-                                </div>
-                            </div>
-                            <!-- Divider -->
-                            <hr>
-                            <!-- Video item END -->
-
-                            <!-- Add topic -->
-                            <a href="#" class="btn btn-sm btn-dark mb-0" data-bs-toggle="modal" data-bs-target="#addTopic"><i class="bi bi-plus-circle me-2"></i>افزودن</a>
-                        </div>
-                        <!-- Topic END -->
-                    </div>
-                </div>
-                <!-- Item END -->
-
-                <!-- Item START -->
-                <div class="accordion-item mb-3">
-                    <h6 class="accordion-header font-base" id="heading-2">
-                        <button class="accordion-button fw-bold rounded d-inline-block collapsed d-block pe-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-2" aria-expanded="false" aria-controls="collapse-2">
-                            آموزش کار به صورت متوسطه
-                        </button>
-                    </h6>
-
-                    <div id="collapse-2" class="accordion-collapse collapse" aria-labelledby="heading-2" data-bs-parent="#accordionExample2">
-                        <div class="accordion-body mt-3">
-                            <!-- Add topic -->
-                            <a href="#" class="btn btn-sm btn-dark mb-0" data-bs-toggle="modal" data-bs-target="#addTopic">
-                                <i class="bi bi-plus-circle me-2"></i>افزودن
-                            </a>
+                            <!-- Topic END -->
                         </div>
                     </div>
-                </div>
-                <!-- Item END -->
-
-                <!-- Item START -->
-                <div class="accordion-item mb-3">
-                    <h6 class="accordion-header font-base" id="heading-3">
-                        <button class="accordion-button fw-bold collapsed rounded d-block pe-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-3" aria-expanded="false" aria-controls="collapse-3">
-                            چقدر باید به فروشندگان پیشنهاد بدهم؟
-                        </button>
-                    </h6>
-                    <!-- Body -->
-                    <div id="collapse-3" class="accordion-collapse collapse" aria-labelledby="heading-3" data-bs-parent="#accordionExample2">
-                        <div class="accordion-body mt-3">
-                            <!-- Add topic -->
-                            <a href="#" class="btn btn-sm btn-dark mb-0" data-bs-toggle="modal" data-bs-target="#addTopic">
-                                <i class="bi bi-plus-circle me-2"></i>افزودن
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <!-- Item END -->
-
+                    <!-- Item END -->
+                @endforeach
             </div>
 
         </div>
     </div>
 
-    <div class="modal fade" id="addLecture" tabindex="-1" aria-labelledby="addLectureLabel" aria-hidden="true">
+    <div class="modal fade" id="addSeason" tabindex="-1" aria-labelledby="addLectureLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header bg-dark">
@@ -121,13 +84,13 @@
                         <!-- Course name -->
                         <div class="col-12">
                             <label class="form-label">نام فصل <span class="text-danger">*</span></label>
-                            <input type="text" class="form-control" placeholder="نام فصل را وارد کنید">
+                            <input wire:model="title" type="text" class="form-control" placeholder="نام فصل را وارد کنید">
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger-soft my-0" data-bs-dismiss="modal">بستن</button>
-                    <button type="button" class="btn btn-success my-0">ذخیره</button>
+                    <button wire:click="addSeason" type="button" class="btn btn-success my-0">ذخیره</button>
                 </div>
             </div>
         </div>
@@ -205,7 +168,7 @@
     </div>
 @push('scripts')
     <script>
-        // Upload image
+        // Upload file
         document.querySelector(".upload-button-file").onclick = function() {
             document.querySelector(".hidden-upload-file").click();
         };
@@ -221,6 +184,12 @@
         document.querySelector(".hidden-upload-mp4").onchange = function() {
             document.querySelector(".upload-name-mp4").value = event.target.files[0].name;
         };
+
+        document.addEventListener('livewire:init',()=>{
+            Livewire.on('closeSeasonModal',()=>{
+                $('#addSeason').modal('toggle')
+            })
+        })
 
     </script>
 @endpush
