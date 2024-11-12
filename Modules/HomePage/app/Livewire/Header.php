ef6f8efce821591e239523f923f093f0a2c8e57f
iman hashemi <hashemi.iman@gmail.com>
2024-11-12 21:29:57 +0330

    home page

---
+++ Modules/HomePage/app/Livewire/Header.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\HomePage\Livewire;
+
+use Livewire\Component;
+
+class Header extends Component
+{
+    public function render()
+    {
+        return view('homepage::livewire.header');
+    }
+}
