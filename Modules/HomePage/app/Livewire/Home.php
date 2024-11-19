4a344a436fa0a362a3e110e4fbfe6ff010fbe5da
iman hashemi <hashemi.iman@gmail.com>
2024-11-19 19:25:31 +0330

    favorite-courses

--- Modules/HomePage/app/Livewire/Home.php
+++ Modules/HomePage/app/Livewire/Home.php
@@ -2,7 +2,9 @@
 
 namespace Modules\HomePage\Livewire;
 
+use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
@@ -12,18 +14,28 @@ use Modules\Category\Models\Category;
 class Home extends Component
 {
 
-    public $selected_tab=7;
+    public $selected_tab;
 
-    public function setSelectedTab($category_id)
+    public function mount(): void
+    {
+       $this->selected_tab = Category::query()
+            ->select('categories.*')
+           ->join('courses','categories.id','=','courses.category_id')
+           ->orderBy('courses.viewed','DESC')
+           ->first()->id;
+    }
+
+    public function setSelectedTab($category_id): void
     {
         $this->selected_tab=$category_id;
     }
     #[Computed]
-    public function categories()
+    public function categories():Collection
     {
         return Category::query()
-//            ->where('parent_id','!=', null)
-            ->whereIn('id',[7,10,11])
+            ->select('categories.*')
+            ->join('courses','categories.id','=','courses.category_id')
+            ->orderBy('courses.viewed','DESC')
             ->limit(5)->get();
     }
     #[Layout('homepage::layouts.master'),Title('صفحه اصلی')]
